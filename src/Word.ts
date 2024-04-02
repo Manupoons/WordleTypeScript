632b41a97a7fb8388ec648eef85d55e4d4d8bc2b
Manuel Maratrat Pons <manuel.maratrat@cuatroochenta.com>
2024-04-02 08:31:07 +0200

    First commit

--- src/Word.ts
+++ src/Word.ts
@@ -1,6 +1,7 @@
 export class Word {
 
     #words: string[];
+
     constructor(wordsArray: string[]){
         this.#words = wordsArray;
     }
@@ -8,11 +9,12 @@ export class Word {
     get words(){
         return this.#words;
     }
+
     set words(wordsArray: string[]){
         this.#words = wordsArray;
     }
 
-    getRandomWord():string {
+    getRandomWord(): string{
         const min = 0;
         const max = this.#words.length-1;
         return this.#words[Math.trunc(Math.random() * (max - min + 1))]
